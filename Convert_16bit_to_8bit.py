f569f5a0e2ca8de7d1394868522aedc43ce43e61
Chris <chris.norton132@gmail.com>
2022-06-18 22:24:10 -0400

    Clean up ignored files

--- Convert_16bit_to_8bit.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import os
-import numpy as np
-
-PATH = "TrainingDataset/output/Images/"
-
-for i in os.listdir(PATH):
-    img = Image.fromarray(np.array(Image.open(PATH + i)).astype("uint8"))
-    train_img = img.save("TrainingDataset/uint8TIF/" + i)
